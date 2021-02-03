6e0f13d18ce24f796582f5bf5dd78e2d65fa327a
SIRI <781188573@qq.com>
2021-02-03 20:34:42 +0800

    1b

--- server.js
+++ server.js
@@ -25,21 +25,15 @@ var server = http.createServer(function(request, response){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(`
-    <!DOCTYPE html>
-    <html lang="zh-CN">
+    <!DOCTYPE html> 
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>node.js</title>
         <link rel="stylesheet" href="/x">
     </head>
     <body>
         <h1>第一次部署node.js</h1>
     </body>
-    </html>
-
-    `)
+   
+  `)
     response.end()
   } else if(path === '/x'){
     response.statusCode = 200
